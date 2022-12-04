b1342ad817cba386dfa5e23ad04bf1623ead3748
Dmitry Brant <me@dmitrybrant.com>
2022-12-03 19:57:25 -0500

    Remove ILRepack.

--- publish.bat
+++ publish.bat
@@ -1,1 +1,1 @@
-E:\git\OutlookMailViewer>dotnet publish -p:PublishSingleFile=true -r win-x64 -c Release --self-contained false OutlookMailViewer\OutlookMailViewer.csproj
+dotnet publish -p:PublishSingleFile=true -r win-x64 -c Release --self-contained false OutlookMailViewer\OutlookMailViewer.csproj
